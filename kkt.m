d38eb7383e8cea2e3d73958d74f4c2237045acc1
Daniel Cao Silva <47014112+DanielCaoSilva@users.noreply.github.com>
2021-11-04 13:07:14 -0700

    Add files via upload
    more general

--- kkt.m
+++ kkt.m
@@ -4,17 +4,46 @@ clc;
 
 format long
 
+%NLP
+f = @(x1,x2) x1.^4 + x2.^4 + 12*x1^2 + 6*x2.^2 -x1.*x2 -x1-x2;
+g.c1 = @(x1,x2) -x1 - x2 +6;
+g.c2 = @(x1,x2) -2*x1 +x2 +3;
+
+syms x1 x2 u1 u2 s1 s2
+L = @(x1,x2,u1,u2,s1,s2) f(x1,x2) + u1.*(g.c1(x1,x2) + s1) + u2.*(g.c2(x1,x2) + s2);
+Lsym = sym(L);
+stationarity = [diff(Lsym,x1);
+                diff(Lsym,x2);
+                diff(Lsym,u1);
+                diff(Lsym,u2)];
+                %diff(Lsym,s1);
+                %diff(Lsym,s2)];
+
+kktSys.case1 = matlabFunction(subForZero(stationarity,u1,u2));
+kktSys.case2 = matlabFunction(subForZero(stationarity,u1,s2));
+kktSys.case3 = matlabFunction(subForZero(stationarity,u2,s1));
+kktSys.case4 = matlabFunction(subForZero(stationarity,s1,s2));
+% fsolve(kktSys.case1,[0,0,0,0])
+% feval(kktSys.case1,0,0,0,0)
+            
+
 c = zeros(7,4);
 z = zeros(1,4);
 feasible = zeros(5,4);
 
 % Starting point
 x0 = [0.1; 0.1; 0.1; 0; 0; 0; 0]; % x1, x2, x3, u1, u2, s1, s2
+% x0 = [0.1; 0.1; 0; 0]; %x1 x2 u1 u2 s1 s2
 options = optimset('Algorithm','levenberg-marquardt');
 c(:,1) = fsolve(@kktsystem_u1u2,x0, options) % Case 1
 c(:,2) = fsolve(@kktsystem_u1s2,x0, options) % Case 2
 c(:,3) = fsolve(@kktsystem_s1u2,x0, options) % Case 3
 c(:,4) = fsolve(@kktsystem_s1s2,x0, options) % Case 4
+
+% c(:,1) = fsolve(@kktSys.case1,x0, options) % Case 1
+% c(:,2) = fsolve(@kktSys.case2,x0, options) % Case 2
+% c(:,3) = fsolve(@kktSys.case3,x0, options) % Case 3
+% c(:,4) = fsolve(@kktSys.case4,x0, options) % Case 4
 
 for i = 1:4
     z(i) = (c(1,i)-1)^2 + c(2,i)^2 + (c(3,i)-2)^2; 
@@ -51,6 +80,10 @@ function [check,opt] = checkCon(vals)
     check = [check1ans;check2ans;check1;check2;totCheck];
 end
 
+function result = subForZero(dL,makeZero1,makeZero2)
+    result = subs(dL,[makeZero1 makeZero2],[0 0]);
+end
+
 function F = kktsystem_u1u2(x) %u1 = u2 = 0
 F = [2*x(1)-2; 
     2*x(2);
@@ -79,3 +112,6 @@ F = [2*x(1)-2+2*x(1)*x(4)+2*x(1)*x(5);
     x(1)^2+x(2)^2+x(3)^2-4; 
     x(1)^2-4*x(2)];
 end
+
+
+
