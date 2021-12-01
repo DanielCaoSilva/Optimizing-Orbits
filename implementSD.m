aa838d31e5a231e1926a58a888a325a2554a205a
Daniel Cao Silva <47014112+DanielCaoSilva@users.noreply.github.com>
2021-11-30 20:33:08 -0800

    Create implementSD.m

---
+++ implementSD.m
@@ -0,0 +1,65 @@
+clear all;
+close all;
+clc;
+
+%Daniel Silva 
+%Steepest Descent 
+
+gd = load('gradientProj.mat');
+g = gd.gd;
+d = gd.d;
+g = matlabFunction(g);
+d = matlabFunction(d);
+
+global ta tb
+syms ta tb
+
+lambda_bar = 1;
+epsil = .01;
+x_k = [-6.6;5];
+k = 1;
+check = 0;
+%subs(g,[ta;tb],x_k)
+%norm(subs(g,[ta;tb],x_k(:,k)))
+%while(norm(subs(g,[ta;tb],x_k(:,k)))>=epsil)
+while(norm(eval(g,x_k,k))>=epsil)
+%for(i = 1:10000)
+    d_k = -1*eval(g,x_k,k);
+    %d_k = -1*sub(g,x_k(:,k));
+    if(d_k == 0)
+        break;
+    end
+    t = 1;
+    bool = 1;
+    while(bool)
+        lbt = lambda_bar/(2.^t);
+        in = x_k(:,k) + lbt*d_k;
+        %L = double(sub(d,in));
+        L = feval(d,in(1),in(2));
+        %U = double(sub(d,x_k(:,k)) + lbt * epsil * sub(g,x_k(:,k)));
+        U = eval(d,x_k,k)+lbt*epsil*eval(g,x_k,k);
+        check = check + 1;
+        if(L<=U)
+            bool = 0;
+            t = t - 1;
+            
+        end
+        t = t+1;
+    end
+    %disp(x_k(:,k));
+    %disp(double(norm(subs(g,[ta;tb],x_k(:,k)))));
+    lambda_k = lbt;
+    x_k(:,k+1) = x_k(:,k)+lambda_k*d_k;
+    k = k + 1;
+end
+
+distVal=d(x_k(1,end),x_k(2,end));
+x_k(:,end)
+
+function r = sub(f,x_k)
+    syms ta tb
+    r = subs(f,[ta;tb],x_k);
+end
+function r = eval(f,x_k,k)
+    r = f(x_k(1,k),x_k(2,k));
+end
